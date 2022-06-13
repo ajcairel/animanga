0d95b33c9954ec6138b11660364cb3c54749bb3b
ajcairel <alexanderjcairel@gmail.com>
2022-06-13 03:13:45 -0700

    Sleepy

--- src/components/Cards/ProfileAnimeCard.jsx
+++ src/components/Cards/ProfileAnimeCard.jsx
@@ -1,16 +1,18 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 export default function ProfileAnimeCard({ anime }) {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={anime.image} />
-      <Card.Body>
-        <Card.Title>{anime.title}</Card.Title>
-      </Card.Body>
-      <Card.Body>
-        <Link to={`/anime/${anime.animeId}`}>Anime Details</Link>
-      </Card.Body>
+    <Card style={{ width: "15rem" }}>
+      <Card.Body style={{ height: "5rem", overflow: "scroll"}}>
+          <Card.Title>{anime.title}</Card.Title>
+        </Card.Body>
+      <Card.Img variant="top" src={anime.image} style={{ height: "20rem", paddingBottom: "1rem" }}/>
+      <Button as={Link} to={`/anime/${anime.animeId}`} variant="light" className="button">
+          {" "}
+          Anime Details
+        </Button>
     </Card>
   );
 }
--- src/components/Cards/ProfileMangaCard.jsx
+++ src/components/Cards/ProfileMangaCard.jsx
@@ -1,16 +1,19 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 export default function ProfileMangaCard({ manga }) {
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={manga.image} />
-      <Card.Body>
-        <Card.Title>{manga.title}</Card.Title>
-      </Card.Body>
-      <Card.Body>
-        <Link to={`/manga/${manga.mangaId}`}>Manga Details</Link>
-      </Card.Body>
+    <Card style={{ width: "15rem" }}>
+      <Card.Body style={{ height: "5rem", overflow: "scroll"}}>
+          <Card.Title>{manga.title}</Card.Title>
+        </Card.Body>
+      <Card.Img variant="top" src={manga.image} style={{ height: "20rem", paddingBottom: "1rem" }} />
+   
+      <Button as={Link} to={`/manga/${manga.mangaId}`} variant="light" className="button">
+          {" "}
+          Manga Details
+        </Button>
     </Card>
   );
 }
